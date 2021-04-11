7c3795911adda58aea768a47a4970138e540edc4
scaringHome <scaring22@gmail.com>
2021-04-11 06:51:04 +0200

    updated validation to mortgage

--- src/App.java
+++ src/App.java
@@ -2,25 +2,58 @@
 import java.text.NumberFormat;
 import java.util.Locale;
 import java.util.Scanner;
-// import java.util.Arrays;
-// import java.util.Date;
 
 public class App {
     public static void main(String[] args) {
         final byte PERCENT = 100;
         final byte MONTH_IN_YEAR = 12;
+        final short MIN_PRINCIPAL = 1000;
+        final int MAX_PRINCIPAL = 1000000;
+        final byte MIN_RATE = 0;
+        final byte MAX_RATE = 30;
+        final byte MIN_PERIOD = 1;
+        final byte MAX_PERIOD = 30;
+
+        int principal;
+        float yearRate;
+        byte period;
 
         Scanner scan = new Scanner(System.in);
-        System.out.print("Principal: ");
 
-        int principal = scan.nextInt();
-        System.out.print("Annual Interest Rate: ");
+        while (true) {
+            System.out.print("Principal: ");
+            principal = scan.nextInt();
 
-        float yearRate = scan.nextFloat();
+            if (principal < MIN_PRINCIPAL || principal > MAX_PRINCIPAL) {
+                System.out.println("Enter number between 1,000 and 1,000,000.");
+                continue;
+            }
+            break;
+        }
+
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            yearRate = scan.nextFloat();
+
+            if (yearRate <= MIN_RATE || yearRate > MAX_RATE) {
+                System.out.println("Enter a value greater than 0 and less than or equal to 30.");
+                continue;
+            }
+            break;
+        }
         float monthRate = yearRate / PERCENT / MONTH_IN_YEAR;
 
-        System.out.print("Period (Years): ");
-        byte period = scan.nextByte();
+        while (true) {
+            System.out.print("Period (Years): ");
+            period = scan.nextByte();
+
+            if (period < MIN_PERIOD || period > MAX_PERIOD) {
+                System.out.println("Enter a value between 1 and 30.");
+                continue;
+            }
+            break;
+        }
+
         int paymants = period * MONTH_IN_YEAR;
         scan.close();
 
@@ -34,19 +67,3 @@ public class App {
         System.out.println("Mortgage: " + formatedMortgage);
     }
 }
-
-// byte myAge = 30;
-// long viewsCount = 3_123_456_789L;
-// float price = 10.99F;
-// char letter = 'A';
-// boolean isTrue = true;
-// Date now = new Date();
-// now. getTime();
-// String mes = "Hell";
-// int[] nums = { 1, 2, 3, 6, 5, 4, 7, 8 };
-// NumberFormat cur = NumberFormat.getCurrencyInstance();
-// String res = NumberFormat.getPercentInstance().format(0.1);
-// Scanner scan = new Scanner(System.in);
-// System.out.print("Name: ");
-// String name = scan.nextLine().trim();
-// System.out.println("You are " + name);
--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class FizzBazz {
+public class FizzBuzz {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         byte inputNumber;
